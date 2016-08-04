e0ee7e14f7d5c3b25b109331601b9f76527393e8
JayChase <JonathanChase@outlook.com>
2016-08-04 17:28:42 +0900

    improved layout

--- app/main.js
+++ app/main.js
@@ -1,8 +1,10 @@
 "use strict";
+var core_1 = require('@angular/core');
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var forms_1 = require('@angular/forms');
 var app_routes_1 = require('./app.routes');
 var app_component_1 = require('./app.component');
+core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
     forms_1.disableDeprecatedForms(),
     forms_1.provideForms(),
--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,13 @@
+
+import {enableProdMode} from '@angular/core';
+
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 import {AppComponent} from './app.component';
+
+enableProdMode();
 
 bootstrap(AppComponent, [
   disableDeprecatedForms(),

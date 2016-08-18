fe70508c3e9f25e00e7a7cf8e48d7bde5a214937
JayChase <JonathanChase@outlook.com>
2016-08-18 15:07:47 +0900

    added core.css to fix material ripple bug

--- app/main.js
+++ app/main.js
@@ -1,17 +1,7 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app.module');
+var core_1 = require('@angular/core');
+core_1.enableProdMode();
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
-// import {enableProdMode} from '@angular/core';
-// import {bootstrap}    from '@angular/platform-browser-dynamic';
-// import { disableDeprecatedForms, provideForms } from '@angular/forms';
-// import { APP_ROUTER_PROVIDERS } from './app.routes';
-// import {AppComponent} from './app.component';
-// enableProdMode();
-// bootstrap(AppComponent, [
-//   disableDeprecatedForms(),
-//   provideForms(),
-//   APP_ROUTER_PROVIDERS
-// ])
-//   .catch(err => console.error(err)); 
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,21 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }              from './app.module';
 
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
+
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-// import {enableProdMode} from '@angular/core';
-
-// import {bootstrap}    from '@angular/platform-browser-dynamic';
-// import { disableDeprecatedForms, provideForms } from '@angular/forms';
-// import { APP_ROUTER_PROVIDERS } from './app.routes';
-
-// import {AppComponent} from './app.component';
-
-// enableProdMode();
-
-// bootstrap(AppComponent, [
-//   disableDeprecatedForms(),
-//   provideForms(),
-//   APP_ROUTER_PROVIDERS
-// ])
-//   .catch(err => console.error(err));
